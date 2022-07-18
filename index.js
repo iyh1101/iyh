044e30230a8adf42a9a37fb0fe2425e2d7114fd4
iyh1101 <108668789+iyh1101@users.noreply.github.com>
2022-07-18 18:26:59 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var time_now = new Date();
+document.write(time_now.getFullYear() + "년 ");
+document.write(time_now.getMonth() + 1 + "월 ");
+document.write(time_now.getDate() + "일 ");
+document.write(time_now.getHours() + "시 ");
+document.write(time_now.getMinutes() + "분 ");
+document.write(time_now.getSeconds() + "초 ");
+document.write(time_now.getMilliseconds() + "밀리초 ");
